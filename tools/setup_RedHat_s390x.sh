a5b78f93ab8b3e61f9b0379d30a52071601d1bce
Huang Rui <bjhuangr@cn.ibm.com>
2018-12-06 10:49:52 +0800

    updates for s390x redhat
    Change-Id: I9cc4f221f9ea7354932dd26df317da3000729c05

---
+++ tools/setup_RedHat_s390x.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+set -o xtrace
+set -o errexit
+
+wget https://download.docker.com/linux/static/stable/s390x/docker-18.06.1-ce.tgz
+tar xzvf docker-18.06.1-ce.tgz
+sudo cp docker/* /usr/bin/
+
+# Setup Docker
+sudo tee /etc/systemd/system/docker.servic << EOF
+[Unit]
+Description=Docker Application Container Engine
+Documentation=https://docs.docker.com
+After=network-online.target firewalld.service
+Wants=network-online.target
+
+[Service]
+Type=notify
+# the default is not to use systemd for cgroups because the delegate issues still
+# exists and systemd currently does not support the cgroup feature set required
+# for containers run by docker
+ExecStart=/usr/bin/dockerd --insecure-registry 9.115.112.35:5001
+ExecReload=/bin/kill -s HUP $MAINPID
+# Having non-zero Limit*s causes performance problems due to accounting overhead
+# in the kernel. We recommend using cgroups to do container-local accounting.
+LimitNOFILE=infinity
+LimitNPROC=infinity
+LimitCORE=infinity
+# Uncomment TasksMax if your systemd version supports it.
+# Only systemd 226 and above support this version.
+#TasksMax=infinity
+TimeoutStartSec=0
+# set delegate yes so that systemd does not reset the cgroups of docker containers
+Delegate=yes
+# kill only the docker process, not all processes in the cgroup
+KillMode=process
+# restart the docker process if it exits prematurely
+Restart=on-failure
+StartLimitBurst=3
+StartLimitInterval=60s
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
+
+sudo systemctl start docker
+sudo docker info
+
+echo "Completed $0."
